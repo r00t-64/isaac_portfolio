3beccb326168d5e0b0b1f98e574299140ed70737
root64 <ariveras@uni.pe>
2023-11-15 20:00:18 -0500

    email sender fixed

---
+++ engine/SendMailEngine.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const sendEmailEngine = (formData) => {
+  // Convert form data into a string
+  const formDataString = `
+    Name: ${formData.name}
+    Email: ${formData.email}
+    Phone: ${formData.phone || 'Not provided'}
+    Message: ${formData.message}
+  `;
+
+  const requestData = {
+    to: 'ariveras5656@gmail.com',
+    subject: 'Test Subject',
+    text: formDataString,
+    name: formData.name,
+    email: formData.email,
+    phone: formData.phone,
+    message: formData.message,
+  };
+
+  return fetch('http://158.101.112.4:3325/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      // Handle success, e.g., show a success message to the user
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // Handle error, e.g., show an error message to the user
+    });
+};
+
+export default sendEmailEngine;
--- src/partials/Contact.jsx
+++ src/partials/Contact.jsx
@@ -1,44 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ContactPic from '../images/contact1.png';
+import sendEmailEngine from '../../engine/SendMailEngine';
 
 function Contact() {
-  return (
-    <section class="contact" id="contact">
-    <h2 class="heading"><i class="fas fa-headset"></i> Get in <span>Touch</span></h2>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
 
-    <div class="container">
-        <div class="content">
-        <div class="image-box">
-            <img draggable="false" src={ContactPic} alt=""/>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the sendEmailEngine function with the form data
+    sendEmailEngine(formData);
+  };
+
+  return (
+    <section className="contact" id="contact">
+      <h2 className="heading">
+        <i className="fas fa-headset"></i> Get in <span>Touch</span>
+      </h2>
+
+      <div className="container">
+        <div className="content">
+          <div className="image-box">
+            <img draggable="false" src={ContactPic} alt="" />
+          </div>
+          <form id="contact-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <div className="field">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+                <i className="fas fa-user"></i>
+              </div>
+              <div className="field">
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                <i className="fas fa-envelope"></i>
+              </div>
+              <div className="field">
+                <input
+                  type="text"
+                  name="phone"
+                  placeholder="Phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+                <i className="fas fa-phone-alt"></i>
+              </div>
+              <div className="message">
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+                <i className="fas fa-comment-dots"></i>
+              </div>
+            </div>
+            <div className="button-area">
+              <button type="submit">
+                Submit <i className="fa fa-paper-plane"></i>
+              </button>
+            </div>
+          </form>
         </div>
-        <form id="contact-form">
-        
-        <div class="form-group">
-            <div class="field">
-            <input type="text" name="name" placeholder="Name" required/>
-            <i class='fas fa-user'></i>
-            </div>
-            <div class="field">
-            <input type="text" name="email" placeholder="Email" required/>
-            <i class='fas fa-envelope'></i>
-            </div>
-            <div class="field">
-            <input type="text" name="phone" placeholder="Phone"/>
-            <i class='fas fa-phone-alt'></i>
-            </div>
-            <div class="message">
-            <textarea placeholder="Message" name="message" required></textarea>
-            <i class="fas fa-comment-dots"></i>
-            </div>
-            </div>
-        <div class="button-area">
-            <button type="submit">
-            Submit <i class="fa fa-paper-plane"></i></button>
-        </div>
-        </form>
-    </div>
-    </div>
+      </div>
     </section>
   );
 }
